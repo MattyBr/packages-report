799a627be8cb1722767bfa98e32af69e566f98d3
Bargstadt Matthew G <BargstadtMatthewG@JohnDeere.com>
2018-02-01 13:51:18 -0600

    Example 1 commit and push to Github

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,15 +1,18 @@
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
-
+ipt <- installed.packages() %>%
+  as_tibble()
 ## keep the variables
 ##   * Package
 ##   * LibPath
 ##   * Version
 ##   * Priority
 ##   * Built
-
+ipt2 <- data.frame(ipt$Package, ipt$LibPath, ipt$Version, ipt$Priority, ipt$Built)
+colnames(ipt2) <- c("Package", "LibPath", "Version", "Priority", "Built")
 ## write it to data/installed-packages.csv
+write.csv(ipt2, here("data/installed-packages.csv"))
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
